a91b9b80afd1ccbfb32936c932ff8f8388ea9432
unknown <hermano.mark.mh@gmail.com>
2019-12-04 17:43:53 +0800

    start github finder, reactsetup

---
+++ github-finder/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment, Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        const name = 'John Doe';
+        const loading = false;
+        const showName = true;
+
+        return (
+            <div className='App'>
+                {loading ? (
+                    <h4>Loading....</h4>
+                ) : (
+                    <h1>Hello {showName && name}</h1>
+                )}
+            </div>
+        );
+    }
+}
+
+export default App;
