3c8f9d72828191d450047c433c01e7fb8609edaf
unknown <hermano.mark.mh@gmail.com>
2019-12-05 17:29:57 +0800

    components, state and props

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,19 +1,35 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-    render() {
-        const name = 'John Doe';
-        const loading = false;
-        const showName = true;
+    state = {
+        users: [],
+        loading: false
+    };
 
+    async componentDidMount() {
+        this.setState({ loading: true });
+
+        const res = await axios.get(
+            `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+
+        this.setState({ users: res.data, loading: false });
+    }
+
+    render() {
         return (
             <div className='App'>
-                {loading ? (
-                    <h4>Loading....</h4>
-                ) : (
-                    <h1>Hello {showName && name}</h1>
-                )}
+                <Navbar />
+                <div className='container'>
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
+                </div>
             </div>
         );
     }
