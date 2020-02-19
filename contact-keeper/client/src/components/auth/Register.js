2d42caae734c3fd548bb40071c8b07b4eac0d7f1
unknown <hermano.mark.mh@gmail.com>
2020-02-19 19:15:43 +0800

    auth context, initial state, register& login forms, alert context, state & component, user registration

---
+++ contact-keeper/client/src/components/auth/Register.js
@@ -0,0 +1,104 @@
+import React, { useState, useContext, useEffect } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
+
+const Register = () => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+
+  const { register, error, clearErrors } = authContext;
+
+  useEffect(() => {
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error]);
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register({
+        name,
+        email,
+        password
+      });
+    }
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              name='name'
+              id=''
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              type='email'
+              name='email'
+              id=''
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id=''
+              value={password}
+              onChange={onChange}
+              minLength='6'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='passsword2'>Cofirm Password</label>
+            <input
+              type='password'
+              name='password2'
+              id=''
+              value={password2}
+              onChange={onChange}
+              minLength='6'
+            />
+          </div>
+          <input
+            type='submit'
+            value='Register'
+            className='btn btn-primary btn-block'
+          />
+        </form>
+      </h1>
+    </div>
+  );
+};
+
+export default Register;
